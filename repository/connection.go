94793573312cad6a4c5cf80c9521305b4ba5d19d
Martin Sidorov <martin.sidorov27@gmail.com>
2023-02-24 17:00:49 +0200

    Handle errors in user repository and create separate connection

---
+++ repository/connection.go
@@ -0,0 +1,39 @@
+package repository
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+	"os"
+
+	_ "github.com/lib/pq"
+)
+
+var DB *sql.DB
+
+// InitDB will initialize the database
+func InitDB() *sql.DB {
+	psqlInfo := fmt.Sprintf(
+		"host=%s port=%s user=%s "+"password=%s dbname=%s sslmode=disable",
+		os.Getenv("POSTGRES_DB_HOST"),
+		os.Getenv("POSTGRES_DB_PORT"),
+		os.Getenv("POSTGRES_DB_USER"),
+		os.Getenv("POSTGRES_DB_PASS"),
+		os.Getenv("POSTGRES_DB_NAME"),
+	)
+
+	db, err := sql.Open("postgres", psqlInfo)
+	if err != nil {
+		log.Fatalf("Unable to connect to database. %v", err)
+	}
+
+	err = db.Ping()
+	if err != nil {
+		log.Fatalf("Unable to connect to database. %v", err)
+	}
+
+	fmt.Println("DB successfully connected!")
+	DB = db
+
+	return db
+}
--- repository/user.go
+++ repository/user.go
@@ -1,44 +1,11 @@
 package repository
 
 import (
-	"database/sql"
-	"fmt"
-	"log"
-	"os"
-
 	"github.com/entain-test-task/model"
 	"github.com/go-openapi/strfmt"
 	_ "github.com/lib/pq"
+	"github.com/pkg/errors"
 )
-
-var DB *sql.DB
-
-// InitDB will initialize the database
-func InitDB() *sql.DB {
-	psqlInfo := fmt.Sprintf(
-		"host=%s port=%s user=%s "+"password=%s dbname=%s sslmode=disable",
-		os.Getenv("POSTGRES_DB_HOST"),
-		os.Getenv("POSTGRES_DB_PORT"),
-		os.Getenv("POSTGRES_DB_USER"),
-		os.Getenv("POSTGRES_DB_PASS"),
-		os.Getenv("POSTGRES_DB_NAME"),
-	)
-
-	db, err := sql.Open("postgres", psqlInfo)
-	if err != nil {
-		log.Fatalf("Unable to connect to database. %v", err)
-	}
-
-	err = db.Ping()
-	if err != nil {
-		log.Fatalf("Unable to connect to database. %v", err)
-	}
-
-	fmt.Println("DB successfully connected!")
-	DB = db
-
-	return db
-}
 
 // GetAllUsers will return all the users in the database
 func GetAllUsers() ([]model.User, error) {
@@ -51,14 +18,14 @@ func GetAllUsers() ([]model.User, error) {
 			users
 	`)
 	if err != nil {
-		return nil, err
+		return nil, errors.Wrap(err, "failed to get all users")
 	}
 
 	for rows.Next() {
 		var user model.User
 		err := rows.Scan(&user.ID, &user.Balance, &user.CreatedAt, &user.UpdatedAt)
 		if err != nil {
-			return nil, err
+			return nil, errors.Wrap(err, "failed to scan user")
 		}
 
 		users = append(users, user)
@@ -68,7 +35,7 @@ func GetAllUsers() ([]model.User, error) {
 }
 
 // GetUser will return a user by id
-func GetUser(userID strfmt.UUID4) (model.User, error) {
+func GetUser(userID strfmt.UUID4) (*model.User, error) {
 	var user model.User
 
 	err := DB.QueryRow(`
@@ -82,8 +49,8 @@ func GetUser(userID strfmt.UUID4) (model.User, error) {
 		userID,
 	).Scan(&user.ID, &user.Balance, &user.CreatedAt, &user.UpdatedAt)
 	if err != nil {
-		return model.User{}, err
+		return nil, errors.Wrap(err, "failed to get user")
 	}
 
-	return user, nil
+	return &user, nil
 }
